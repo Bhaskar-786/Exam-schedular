0d46e06045d48c64e8b1fe8a2c7d5ef4438874f3
Bhaskar-786 <hhhbhaskar9@gmail.com>
2024-10-13 12:39:56 +0530

    writing constraints file...

--- constraints.py
+++ constraints.py
@@ -1,1 +1,25 @@
-from models import Course, Color
+from models.course import Course
+from models.color import Color
+from models.student import Student
+from models.lecture_hall import LectureHall
+from typing import Set
+
+
+class Constraints:
+     
+    def __init__(self, scheduler):
+        self.scheduler = scheduler
+
+
+    def is_suitable(self, course: Course, color: Color) -> bool:
+        if not self.room_capacity(course, color):
+            return False
+        if not self.no_exam_clashes(course, color):
+            return False
+        if not self.no_consecutive_exams(course, color):
+            return False
+        if not self.maximum_exams_per_day(course, color):
+            return False
+        if not self.minimum_gap(course, color):
+            return False
+        return True
